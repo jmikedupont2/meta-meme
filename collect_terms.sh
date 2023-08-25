bb2ece34e044425b734b620f05538e65e7bd55f5
mike dupont <mike.dupont@introspector.local>
2023-08-25 05:32:40 -0400

    adding new embed

--- collect_terms.sh
+++ collect_terms.sh
@@ -1,7 +1,11 @@
-  528  cd meta-meme/
-  526  git log --all > patch.txt
-  528  cd meta-meme.wiki/
-  529  git log --all > patch.txt
-  533  cp ../meta-meme.wiki/patch.txt wiki-patch.txt
-  538  cat wiki-patch.txt patch.txt >total.txt
-  545  grep -P  -o -e '([a-zA-Z0-9]+)' total.txt  |sort |uniq -c | sort -n >tokens.txt
+pushd meta-meme/
+git log --all --patch > patch.txt
+popd
+pushd cd meta-meme.wiki
+git log --patch --all > patch.txt
+popd
+cp ./meta-meme.wiki/patch.txt meta-meme/wiki-patch.txt
+pushd meta-meme/
+cat wiki-patch.txt patch.txt >total.txt
+grep -P  -o -e '([a-zA-Z0-9]+)' total.txt  |sort |uniq -c | sort -n >tokens.txt
+popd
