c77480b7495d257d70520b86e6d5bc4358009bc0
mike dupont <mike.dupont@introspector.local>
2023-09-10 07:59:34 -0400

    adding scripts

---
+++ scripts/emojis.sh
@@ -0,0 +1,4 @@
+  emojintrospector read -i ./review_protobufv2.md > review_protobufv2.emojis 
+  emojintrospector read -i ./review_protobufv2.emojis   > ./review_protobufv2.emojis2 
+  emojintrospector read -i ./review_protobufv2.emojis2   > ./review_protobufv2.emojis3
+  emojintrospector read -i ./review_protobufv2.emojis3   > ./review_protobufv2.emojis4
